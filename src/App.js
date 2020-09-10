c65ccf6373d14f4a9395a6626ac664922f73676d
sulemanbutt5 <sulemanbutt5@gmail.com>
2020-09-10 14:38:50 +0500

    Final Convertor 1.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import CC from './CC'
+
+const url = 'https://api.exchangeratesapi.io/latest'
 
 function App() {
+  const [cur, setCur] = useState([])
+  const [from, setFrom] = useState()
+  const [to, setTo] = useState()
+  const [exR, setExR] = useState()
+  const [amount, setAmount] = useState(1)
+  const [IsFrom, setIsFrom] = useState(true)
+
+  let toAmount, fromAmount
+  if (IsFrom) {
+    fromAmount = amount
+    toAmount = amount * exR
+  } else {
+    toAmount = amount
+    fromAmount = amount / exR
+  }
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        const firstCurrency = Object.keys(data.rates)[0]
+        setCur([data.base, ...Object.keys(data.rates)])
+        setFrom(data.base)
+        setTo(firstCurrency)
+        setExR(data.rates[firstCurrency])
+      })
+  }, [])
+
+  useEffect(() => {
+    if (from != null && to != null) {
+      fetch(`${url}?base=${from}&symbols=${to}`)
+        .then(res => res.json())
+        .then(data => setExR(data.rates[to]))
+    }
+  }, [from, to])
+
+  function handleFromAC(e) {
+    setAmount(e.target.value)
+    setIsFrom(true)
+  }
+
+  function handleToAC(e) {
+    setAmount(e.target.value)
+    setIsFrom(false)
+  }
+
   return (
-    <div>FINAL CONVERTOR</div>
+    <>
+      <h1>Final Convertor</h1>
+      <CC className="tofrom"
+        currencyOptions={cur}
+        selectedCurrency={from}
+        onChangeCurrency={e => setFrom(e.target.value)}
+        onChangeAmount={handleFromAC}
+        amount={fromAmount}
+      />
+      <div className="equals">=</div>
+      <CC className="tofrom"
+        currencyOptions={cur}
+        selectedCurrency={to}
+        onChangeCurrency={e => setTo(e.target.value)}
+        onChangeAmount={handleToAC}
+        amount={toAmount}
+      />
+    </>
   );
 }
 
